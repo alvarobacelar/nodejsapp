2c30e9186d96c6fd37099c3ac9ebf1b0d57bff16
Álvaro Bacelar <alvaro@alvarobacelar.com>
2018-06-04 17:43:31 -0300

    Adicionando a versão 2 do projeto

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:wheezy
+FROM node:6.14
 
 WORKDIR /usr/src/app
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-#build app
-
 cd /usr/src/app
-
-npm install
-
-bower cache clean
-
-bower install
 
 grunt build
 
